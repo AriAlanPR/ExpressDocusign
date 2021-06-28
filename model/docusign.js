779a86f3f56ef0648a5ecac2526fcd37beeb6fe2
Ari Alan Pérez Romero <shaymin2003@gmail.com>
2021-06-28 11:34:57 -0500

    Update docusign.js

--- model/docusign.js
+++ model/docusign.js
@@ -5,10 +5,10 @@ const requester = require('./requester');
 const moment = require('moment');
 const docusign = require('docusign-esign');
 
-const account_id = '36da5cd1-fa11-4354-b9db-eeb1cc682914';
-const secret_key = '9ebdcd12-18b7-4927-a85f-1cd3e92d269a';
-const integration_key = '0bab569b-8ab4-45c6-9c4d-133b7d438cf9';
-const state = 'ykv2XLx1BpT5Q0F3MRPHb94j';
+const account_id = '';
+const secret_key = '';
+const integration_key = '';
+const state = '';
 const scope = 'signature';
 const base_path = 'https://demo.docusign.net/';
 const base_auth_url = 'https://account-d.docusign.com/oauth/';
@@ -45,7 +45,7 @@ var get_Consent = () => {
                         "&scope=" + scope +
                         "&client_id=" + integration_key + 
                         "&state=" + state +
-                        "&redirect_uri=" + encodeURIComponent( 'http://localhost:3000/docusign/callback' );
+                        "&redirect_uri=" + encodeURIComponent( 'http://...' );
 
     return bodytosend;
 }
@@ -83,9 +83,9 @@ var authorize_Token = async (code) => {
 }
 
 var token_refresh = {
-    "access_token": "eyJ0eXAiOiJNVCIsImFsZyI6IlJTMjU2Iiwia2lkIjoiNjgxODVmZjEtNGU1MS00Y2U5LWFmMWMtNjg5ODEyMjAzMzE3In0.AQoAAAABAAUABwAA4YzLsozYSAgAACGw2fWM2EgCAMeumPlr911Lsz7qZ2tDThMVAAEAAAAYAAEAAAAFAAAADQAkAAAAMGJhYjU2OWItOGFiNC00NWM2LTljNGQtMTMzYjdkNDM4Y2Y5IgAkAAAAMGJhYjU2OWItOGFiNC00NWM2LTljNGQtMTMzYjdkNDM4Y2Y5MAAARagksozYSDcAYg93ZiJLkk6BvkQKqUdMJw.T0fnvbwiWHLTrnkH0Gr2_sdTG6PVlUUtphzDpnS-tZFsRYX9wf3hKnaI6lzbds-tgtt1wl8xKIGx7hUxWLisUU59zK2_sTFVz8lhSdJ1evzRdNGw749DVbgkgTwyGmpETpQuvDkb0b64TXFMw6ukkijlwOvQ1UHYGXeZr0pXJYhPVhQjlbGcnY0KwBZ-m155XkZnB1JcgLBMxL3G8KZnNWdNKtIGl7O6mEbqNrDrOG9r4Ucp5uZYKgRds-7mtXpYo8LzSMOyzEh0QM5Eb5Mmgq0CYwkMTbEiutOKfNpDcV_7LMViS1NmtFmVWPJFLz_iWUfMBTVgIuNWNtwHrLzqtw", 
+    "access_token": "", 
     "token_type": "Bearer", 
-    "refresh_token": "eyJ0eXAiOiJNVCIsImFsZyI6IlJTMjU2Iiwia2lkIjoiNjgxODVmZjEtNGU1MS00Y2U5LWFmMWMtNjg5ODEyMjAzMzE3In0.AQoAAAABAAgABwAA4YzLsozYSAgAAGHxw0Wk2EgCAMeumPlr911Lsz7qZ2tDThMVAAEAAAAYAAEAAAAFAAAADQAkAAAAMGJhYjU2OWItOGFiNC00NWM2LTljNGQtMTMzYjdkNDM4Y2Y5IgAkAAAAMGJhYjU2OWItOGFiNC00NWM2LTljNGQtMTMzYjdkNDM4Y2Y5MAAARagksozYSDcAYg93ZiJLkk6BvkQKqUdMJw.k-y4joKAtXgzNfxeICUo9tLPzpV68TEtCiEqiswsRGn08DUImlUbswOdqk70eAzaNGtrgfpyzTJH0QEPJ64otVaP4lkRRkAugcL43QTn61TYnX5r2f03pyGTjxJbVUF-JlheFurN3SWaXO1uzAYdUludtRymf0mgnZvAQjt8qEhRlESIYxF_MbxcTKlUUmSTXJoZmruL4AUQ2Aqbgfsu-aRAk69oRRO0I-7L2h3sX1LfQ1hm_xGdIUS9tUHlUWB3a6H9pNUXxMqmhMPDV_jnFw6stXxfes0O2J6vZpfoBcij877yUW2Q3CiUM1CO1Dm5I3vvZXc0JcNzraaWo3mBxA", 
+    "refresh_token": "", 
     "expires_in": 28800
 }
 
